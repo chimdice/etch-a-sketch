4dd99fdd4f4ce8c2ccb4b2b70315c918d3011403
chimdice <118031439+chimdice@users.noreply.github.com>
2024-05-14 20:11:46 -0700

    Add variable grid size with changing color

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 const container = document.querySelector("#container");
 
-for (i=0; i < 256;i++) {
-    const square = document.createElement("div");
+const btn = document.querySelector("button");
 
-    square.addEventListener('mouseenter', () => {
-        square.style.background = "black";
-    });
-    square.style.outline ="1px solid black";
-    square.style.height = "6.25%";
-    square.style.width = '6.25%';
-    container.appendChild(square);
-}
+btn.addEventListener("click", () => {
+    let newGridSize = prompt("Please Choose a new number of squares on each side of the grid!");
+    currentGridSize = Number(newGridSize);
 
+    while (currentGridSize >= 100) {
+        let newGridSize = prompt("Please Choose a size less than 100!");
+        currentGridSize = Number(newGridSize);
+    }
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    };
+
+    createGrid(currentGridSize);
+});
+
+let currentGridSize = 16
+
+function createGrid (currentGridSize) {
+    
+    for (i=0; i < currentGridSize**2 ;i++) {
+        const square = document.createElement("div");
+
+        square.addEventListener('mouseenter', () => {
+
+            if (square.style.opacity) {
+                let opacity = Number(square.style.opacity);
+                if (opacity < 1) {
+                    console.log(square.style.opacity);
+                    opacity = opacity + 0.1;
+                    square.style.opacity = opacity.toString();
+                }; 
+            } else {
+                square.style.opacity = "0.1"
+
+                r = randomColor()
+                g = randomColor()
+                b = randomColor()
+
+                square.style.background = `rgb(${r}, ${g}, ${b})`;
+            };
+
+        });
+        square.style.outline ="1px solid black";
+        square.style.height = `${100/currentGridSize}%`;
+        square.style.width = `${100/currentGridSize}%`;
+        container.appendChild(square);
+    }
+};
+
+let randomColor = () => {
+    return Math.floor(Math.random()*255)
+};
+
+createGrid(currentGridSize);
